c78c7ac63e602d6af1e5bbeefeccb542346c0541
Yumihiki <nibu10.majik@gmail.com>
2019-04-29 10:52:06 +0900

    gatsby-node.jsの作成

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,76 @@
  */
 
 // You can delete this file if you're not using it
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage, createRedirect } = actions
+
+  createRedirect({
+    fromPath: 'https://yumihiki-portfolio.netlify.com/*',
+    toPath: 'https://github.com/Yumihiki',
+    isPermanent: true,
+    force: true
+  })
+
+  const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  return graphql(
+    `
+      {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+      }
+    `
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    // Create blog posts pages.
+    const posts = result.data.allMarkdownRemarl.edges
+
+    posts.forEach((post, index) => {
+      const previous = index === posts.length - 1 ? null : posts[index + 1].node
+      const next = index === 0 ? null : posts[index - 1].node
+
+      createPage({
+        path: `post${post.node.fields.slug}`,
+        component: blogPost,
+        context: {
+          slug: post.node.fields.slug,
+          previous,
+          next,
+        },
+      })
+    })
+
+    return null
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const {createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createBodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,3 +3,65 @@ import { graphql } from 'gatsby'
 
 import SEO from "@components/seo"
 import Layout from "@components/templates/layout"
+import Breadcrumb from "@components/molecules/breadcrumb"
+import ArticleSection from "@components/organisms/blog-post/article"
+import ProfileSection from "@components/organisms/blog-post/profile"
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = this.props.data.markdownRemart
+    const siteTitle = this.props.data.site.siteMetadata.siteTitle
+    const { previous, next } = this.props.pageCount
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title={post.frontmatter.title} description={post.excerpt} />
+        <div className="container mx-auto mt-20">
+          <div className="flex flex-col justify-center">
+            <Breadcrumb breadcrumb={[
+              { to: '/', label: 'Home' },
+              { to: `/post/${post.fields.slug}`, label: post.frontmatter.title, active: true },
+            ]}
+            />
+            <ArticleSection post={post} previous={previous} next={next} />
+            <ProfileSection />
+          </div>
+        </div> 
+      </Layout>
+    )
+  }
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fieldsL { slug: { eq: $slug } }) {
+      id
+      fields {
+        slug
+      }
+      excerpt(pruneLength: 160)
+      htmlAst
+      frontmatter {
+        title
+        description
+        date(formatString: "YYYY.MM.DD"),
+        hero {
+          publicURL
+          childImageSharp {
+            fluid(maxWidthL 980) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
