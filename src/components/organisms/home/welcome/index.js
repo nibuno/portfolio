c992e2bb8219610a4c64e4f74f42429feeb4ca0f
Yumihiki <nibu10.majik@gmail.com>
2019-09-22 11:32:00 +0900

    レイアウト変更

--- src/components/organisms/home/welcome/index.js
+++ src/components/organisms/home/welcome/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import tw from "tailwind.macro"
 import heroImage from "@contents/images/home-hero-image.jpg"
+import myIcon from "@contents/images/shoutingChickenIcon.jpg"
 import TwitterIcon from "@components/atoms/icon/twitter"
 import GithubIcon from "@components/atoms/icon/github"
 
@@ -20,18 +21,18 @@ const SectionStyled = styled.section`
 
 const Welcome = () => (
   <SectionStyled>
-    <div className="container mx-auto my-24">
-      <div className="flex justify-center items-center px-4 p-10">
+    <div className="container mx-auto my-24 pt-4">
+      <div className="flex justify-center items-center px-4">
         <div className="text-center w-full">
-          <h3 className="text-black text-3xl">Hi, Everyone</h3>
-          <h1 className="text-black font-bold my-5 text-5xl">I'm Yumihiki!</h1>
-          <p className="text-black text-2xl">Designer / PHPer / Kyuder</p>
+          <h3 className="text-black text-3xl">ご訪問ありがとうございます！</h3>
+          {/* <h1 className="text-black font-bold my-5 text-5xl">Welcome!</h1> */}
+          <div className="w-20 h-20 pt-4">
+            <img src={myIcon} alt="Yumihiki"></img>
+          </div>
+          <p className="text-black text-3xl pt-8">I'm Designer / PHPer / Kyuder</p>
           <div className="my-10">
             <a href="https://github.com/Yumihiki" target="_blank" rel="noreferrer noopener" aria-label="github" className="text-black hover:text-grey-darker mr-6">
               <GithubIcon className="fill-current w-10 h-10" />
-            </a>
-            <a href="https://twitter.com/sakura_willow" target="_blank" rel="noreferrer noopener" aria-label="twitter" className="text-blue hover:text-grey-darker mr-6">
-              <TwitterIcon className="fill-current w-10 h-10" />
             </a>
           </div>
         </div>
